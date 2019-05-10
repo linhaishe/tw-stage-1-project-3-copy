754aa962064549662e128d5c37a6c5151ac95710
xusumu <xusumu@gmail.com>
2019-05-10 17:42:36 +0800

    更新

--- whack-a-mole/game-easy.js
+++ whack-a-mole/game-easy.js
@@ -7,12 +7,12 @@ window.onload = function () {//这个开头window.onload的用意？？？？？
     const moles = document.querySelectorAll('.mole');//获取所有地鼠
     const startBtn = document.getElementById('start_btn');
     let titleH1 = document.getElementById('title');
-    //为什么有些颜色是灰色的？
+    //为什么有些颜色是灰色的？因为值没有设置√
 
     let lastHole;
-    let timeUp = false;
+    let timeUp = false;//
     let score = 0;
-    let gameTime = 30000;//30秒，从10秒修改成30秒
+    let gameTime = 10000;//10秒游戏时间
 
     //addEventListener，在文档中添加点击事件。当用户在文档任何地方点击时，在 id="demo" 的 <p> 元素上输出 "Hello World"：
     //document.addEventListener("click", function(){
@@ -35,28 +35,32 @@ window.onload = function () {//这个开头window.onload的用意？？？？？
         }, 700);
     }
 
-    //为什么要先写游戏结束时候的代码？？？？？
+    //游戏开始时候要发生的事件
     function startGame() {
-        resetScoreAndTime();
-        peep();
-        setTimeout(() => {
-            // TODO: 写当游戏时间结束后要发生的事
-        }, gameTime)
+        resetScoreAndTime();//游戏初始化设置
+        peep();//老鼠出洞
+        //箭头函数
+        //setTimeout(JavaScript 函数, 等待的毫秒数)
+        score = 0;
+        timeUp = false;
+        setTimeout(() => timeUp = true,10000);
     }
 
     /**
      * 初始化设置.
      */
     function resetScoreAndTime() {
-        // TODO: 写游戏的初始化设置
+        // TODO: 写游戏的初始化设置，时间和分数的设置
+        scoreBoard.textContent = 0;        
+        setTimeout(() => timeUp = true,10000);
     }
 
     /**
      * 出洞.
      */
     function peep() {
-        const time = randomTime(200, 1000);
-        const hole = randomHole(holes);
+        const time = randomTime(200, 1000);//随机时间出现
+        const hole = randomHole(holes);随机洞口出现
         comeOutAndStop(hole, time);
     }
 
@@ -68,8 +72,9 @@ window.onload = function () {//这个开头window.onload的用意？？？？？
      * @returns {number}
      */
     function randomTime(min, max) {
-        // TODO: 写生成随机数的逻辑，
-        return 0;
+        // TODO: 写生成随机数的逻辑
+        return Math.round(Math.random() * (max - min) + min);
+        //return 0;
     }
 
     /**
@@ -80,7 +85,15 @@ window.onload = function () {//这个开头window.onload的用意？？？？？
      */
     function randomHole(holes) {
         // TODO: 写地鼠随机选择钻出地洞的逻辑，如果与上一个是相同地洞，则重新选择一个地洞.
-        return null;
+        const current = Math.floor(Math.random() * holes.length); 
+        const hole = holes[current];
+        if (hole === lastHole) {
+            return randomHole(holes);
+        }
+
+        lastHole = hole;
+        return hole;
+        //return null;
     }
 
     /**
@@ -91,13 +104,36 @@ window.onload = function () {//这个开头window.onload的用意？？？？？
      */
     function comeOutAndStop(hole, time) {
         // TODO: 写地鼠出洞并停留相应时间，如果游戏时间未结束(timeUp)，继续出洞(peep).
+        hole.classList.add('up');
+
+        setTimeout(() => {
+          hole.classList.remove('up');
+          if(!timeUp) peep();
+        }, time);
     }
 
     /**
      * 打地鼠。为每个moles添加点击事件，点击后分数显示+1，地鼠入洞。
      */
     moles.forEach(mole => mole.addEventListener('click', function (e) {
+        //JavaScript forEach() 方法
         // TODO: 在这里写用户点击地鼠发生的事.
+        //if(timeUp==false){
+            //alert("Please Start Game !");
+           // return;
+        //}
+       // else{
+            //if(document.getElementsByClassName("hole["+current+"]").innerHtml !=""){
+                //score += 1;
+                //scoreBoard.textContent = score;
+                //document.getElementsByClassName("hole["+current+"]")//.innerHtml = '<img src="whack-a-mole\mole.svg">';
+           // }
+
+        //}
+        if(!e.isTrusted) return; // cheater
+        score ++;
+        this.classList.remove('up');
+        scoreBoard.textContent = score;
     }));
 
 };
